1b8775344de517a509065d7ca3b082e5264e6f14
Mike Friesen <mfriesen@gmail.com>
2020-11-01 20:50:10 -0600

    updated

--- serverlessrepo-create-application.sh
+++ serverlessrepo-create-application.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-s3bucket=formkiq-distribution-core
+s3bucket=
 
 declare -a StringArray=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "sa-east-1")
 
@@ -8,7 +8,7 @@ declare -a StringArray=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-sout
 for region in ${StringArray[@]}; do
 
    sed "s/{{awsregion}}/$region/g" build/parima.yml > build/parima-${region}.yml
-   aws cloudformation package --template-file build/parima-${region}.yml --s3-bucket ${s3bucket} --profile formkiq > build/template.tmp
+   aws cloudformation package --template-file build/parima-${region}.yml --s3-bucket ${s3bucket} > build/template.tmp
 
    aws serverlessrepo create-application \
      --author "FormKiQ Inc" \
@@ -21,5 +21,5 @@ for region in ${StringArray[@]}; do
      --readme-body file://README.md \
      --semantic-version "1.3.0" \
      --spdx-license-id "Apache-2.0" \
-     --template-body file://build/template.tmp --profile formkiq
+     --template-body file://build/template.tmp
 done
