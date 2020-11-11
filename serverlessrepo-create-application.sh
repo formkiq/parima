32b8654c2120af95983da12dd6243490030879f8
Mike Friesen <mfriesen@gmail.com>
2020-11-10 20:26:50 -0600

    added source-code-url to create serverless application repository script

--- serverlessrepo-create-application.sh
+++ serverlessrepo-create-application.sh
@@ -2,7 +2,8 @@
 
 s3bucket=
 
-declare -a StringArray=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "sa-east-1")
+#declare -a StringArray=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "sa-east-1")
+declare -a StringArray=("us-east-1" )
 
 # Iterate the string array using for loop
 for region in ${StringArray[@]}; do
@@ -21,5 +22,6 @@ for region in ${StringArray[@]}; do
      --readme-body file://README.md \
      --semantic-version "1.3.0" \
      --spdx-license-id "Apache-2.0" \
+     --source-code-url https://github.com/formkiq/parima \
      --template-body file://build/template.tmp
 done
